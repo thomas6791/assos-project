fcb54f6695a13d4b76c14f3ca09f9ac32f9ef7ee
thomas hunckler <hunckler.thomas@hotmail.fr>
2019-06-26 09:31:57 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'pages/contact'
-  get 'pages/about'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'alentours', to: 'pages#alentours', as: :alentours
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
